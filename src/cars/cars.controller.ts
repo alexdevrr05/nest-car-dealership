f9b83e21fe0fbf709c586adfb4eb68370b69fe07
alexdevrr05 <pv190115553@vallarta.tecmm.edu.mx>
2022-12-19 21:45:01 -0600

    Get request with id param

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,9 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('cars')
 export class CarsController {
+    private cars = ['Chevrolet', 'Toyota', 'Ford'];
+
     @Get()
     getAllCars() {
-        return ['Chevrolet', 'Toyota'];
+        return this.cars;
+    }
+
+    @Get(':id')
+    getCarById(@Param('id') id: string) {
+
+        const car = this.cars[id];
+        const msg = 'There is no car'
+
+        if (!car) return { msg };
+
+        return {
+            car,
+        };
     }
 }
